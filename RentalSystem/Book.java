bd4c49906785530e46aa2788d920371270371584
Javad <jvsadti83@gmail.com>
2023-05-12 21:18:23 +0330

    Book updated

--- RentalSystem/Book.java
+++ RentalSystem/Book.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Book extends Item{
     private String author;
@@ -26,18 +28,5 @@ public class Book extends Item{
         this.publisher = publisher;
     }
 
-    public void rentItem(Customer customer) {
-        this.setAvailable(false);
-        Rental rental = new Rental(customer, this, Integer.parseInt(this.getID() + String.valueOf(customer.getID())));
-        customer.getRentals().add(rental);
-        System.out.println("Book named " + this.getTitle() + " rented");
-    }
-
-    public void returnItem(Rental rental) {
-        this.setAvailable(true);
-        rental.getCustomer().getRentals().remove(rental);
-        System.out.println("Book named " + this.getTitle() + " returned");
-        System.out.println("Your lateFee: " + rental.calculateLateFee() + 'T');
-    }
 
 }
