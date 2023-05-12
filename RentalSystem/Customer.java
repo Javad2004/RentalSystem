0ab8d0cbd9d53ec3df3f5e650a03237ddccc042c
Javad <jvsadti83@gmail.com>
2023-05-12 14:37:50 +0330

    Customer updated

--- RentalSystem/Customer.java
+++ RentalSystem/Customer.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-    private final String name;
-    private final String email;
-    private final String phone_number;
-    private final String address;
-    private final int ID;
-    private final List<Rental> rentals;
+    private String name;
+    private String email;
+    private String phone_number;
+    private String address;
+    private int ID;
+    private List<Rental> rentals;
 
     public Customer(String name, String email, String phone_number, String address, int ID) {
         this.name= name;
@@ -40,5 +40,29 @@ public class Customer {
 
     public List<Rental> getRentals() {
         return rentals;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setRentals(List<Rental> rentals) {
+        this.rentals = rentals;
     }
 }
